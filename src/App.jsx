f6af86231145e7d59dad6a0dd1af3bfac77bcd74
MrAbelDev <abelmruso@gmail.com>
2025-07-17 16:10:51 +0200

    composant tagName

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import Header from "./assets/components/Header";
 import Banner from "./assets/components/Banner";
 import Footer from "./assets/components/Footer";
 import Error from "./assets/components/Error";
+import TagName from "./assets/components/TagName";
 
 function App() {
     return (
         <>
             <Header></Header>
             <Banner></Banner>
+
             <Footer></Footer>
-            <Error></Error>
+            <TagName></TagName>
         </>
     );
 }
