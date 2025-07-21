5815f2694d91814f5910fb6ecb088ca3df247c65
MrAbelDev <abelmruso@gmail.com>
2025-07-21 11:00:13 +0200

    intégration des cards des logements sur la page d'accueil

--- src/assets/components/Cards.jsx
+++ src/assets/components/Cards.jsx
@@ -2,14 +2,17 @@ import data from "../data/logements.json";
 import styles from "../styles/cards.module.scss";
 
 function Cards() {
-    const imagen = data[0].cover;
-    const titulo = data[0].title;
-
     return (
-        <article className={styles.card}>
-            <img src={imagen} alt={titulo} />
-            <h4>{titulo}</h4>
-        </article>
+        <section className={styles.gallery}>
+            {data.map((logement) => {
+                return (
+                    <article className={styles.card}>
+                        <img src={logement.cover} alt={logement.title} />
+                        <h4>{logement.title}</h4>
+                    </article>
+                );
+            })}
+        </section>
     );
 }
 
