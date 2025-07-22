cb9a7e99038efb2061357393eb01f3f1400004b5
MrAbelDev <abelmruso@gmail.com>
2025-07-22 14:35:01 +0200

    mise en page du carrousel

--- src/assets/components/Address.jsx
+++ src/assets/components/Address.jsx
@@ -2,6 +2,7 @@ import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Home from "../../pages/Home";
 import About from "../../pages/About";
+import FicheLogement from "../../pages/FicheLogement";
 import Error404 from "../../pages/Error404";
 /* import FicheLogement from "../../pages/FicheLogement"; */
 
@@ -10,7 +11,7 @@ function Address() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-
+            <Route path="/logements" element={<FicheLogement />} />
             <Route path="*" element={<Error404 />} />
         </Routes>
     );
--- src/pages/FicheLogement.jsx
+++ src/pages/FicheLogement.jsx
@@ -1,3 +1,26 @@
+import arrowBack from "../assets/img/arrow_back.svg";
+import arrowForward from "../assets/img/arrow_forward.svg";
+import logements from "../assets/data/logements.json";
+import styles from "../assets/styles/logements.module.scss";
+
 function FicheLogement() {
-    return {};
+    const defautlImage = logements[0].pictures[0];
+    return (
+        <main>
+            <div className={styles.carrousel}>
+                <img src={defautlImage} alt="" />
+
+                <p>1/4</p>
+
+                <button className={styles.buttonLeft}>
+                    <img src={arrowBack} alt="" />
+                </button>
+                <button className={styles.buttonRight}>
+                    <img src={arrowForward} alt="" />
+                </button>
+            </div>
+        </main>
+    );
 }
+
+export default FicheLogement;
