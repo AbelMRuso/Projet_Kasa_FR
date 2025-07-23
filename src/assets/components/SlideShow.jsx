fd00a5108be8a7a02f7d6bc4a638b2d880acafee
MrAbelDev <abelmruso@gmail.com>
2025-07-23 23:06:58 +0200

    Ajout de useParams, intégration des images dans le carrousel, fonctionnalité bouton arrowForward

---
+++ src/assets/components/SlideShow.jsx
@@ -0,0 +1,39 @@
+import arrowBack from "../img/arrow_back.svg";
+import arrowForward from "../img/arrow_forward.svg";
+import styles from "../styles/logements.module.scss";
+import data from "../data/logements.json";
+import { useState } from "react";
+import { useParams } from "react-router-dom"; //se usa para recuperar la id de la url
+
+function SlideShow() {
+    const { id } = useParams();
+    const logement = data.find((item) => item.id === id);
+
+    const [showImage, setshowImage] = useState(0);
+
+    return (
+        <div className={styles.carrousel}>
+            <img key={showImage} src={logement.pictures[showImage]} alt="" />
+
+            <p>1/4</p>
+
+            <button className={styles.buttonLeft}>
+                <img src={arrowBack} alt="" />
+            </button>
+            <button
+                onClick={() => {
+                    if (showImage >= logement.pictures.length - 1) {
+                        setshowImage(0);
+                    } else {
+                        setshowImage(showImage + 1);
+                    }
+                }}
+                className={styles.buttonRight}
+            >
+                <img src={arrowForward} alt="" />
+            </button>
+        </div>
+    );
+}
+
+export default SlideShow;
--- src/pages/FicheLogement.jsx
+++ src/pages/FicheLogement.jsx
@@ -1,28 +1,18 @@
-import arrowBack from "../assets/img/arrow_back.svg";
-import arrowForward from "../assets/img/arrow_forward.svg";
-import logements from "../assets/data/logements.json";
 import styles from "../assets/styles/logements.module.scss";
 import TagName from "../assets/components/TagName";
 import star from "../assets/img/star_active.png";
 import DropDown from "../assets/components/DropDown";
+import SlideShow from "../assets/components/SlideShow";
+import data from "../assets/data/logements.json";
 
 function FicheLogement() {
-    const defautlImage = logements[0].pictures[0];
-    const hostProfile = logements[0].host.picture;
+    let hostProfile = data[2].host.picture;
     return (
         <main>
-            <div className={styles.carrousel}>
-                <img src={defautlImage} alt="" />
-
-                <p>1/4</p>
-
-                <button className={styles.buttonLeft}>
-                    <img src={arrowBack} alt="" />
-                </button>
-                <button className={styles.buttonRight}>
-                    <img src={arrowForward} alt="" />
-                </button>
+            <div>
+                <SlideShow></SlideShow>
             </div>
+
             <div>
                 <div className={styles.contenairInfo}>
                     <div>
