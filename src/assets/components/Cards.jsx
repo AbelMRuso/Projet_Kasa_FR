3432282ab446424104bf4fa3ea6ca2e9366de7b9
MrAbelDev <abelmruso@gmail.com>
2025-07-23 12:51:55 +0200

    implémentation de l'envoi vers la page FicheLogement en cliquant sur une card d'hébergement en recuperant son id

--- src/assets/components/Cards.jsx
+++ src/assets/components/Cards.jsx
@@ -1,15 +1,18 @@
 import data from "../data/logements.json";
 import styles from "../styles/cards.module.scss";
+import { NavLink } from "react-router-dom";
 
 function Cards() {
     return (
         <section className={styles.gallery}>
             {data.map((logement) => {
                 return (
-                    <article className={styles.card}>
-                        <img src={logement.cover} alt={logement.title} />
-                        <h2>{logement.title}</h2>
-                    </article>
+                    <NavLink to={`/logements/${logement.id}`}>
+                        <article className={styles.card} key={logement.id}>
+                            <img src={logement.cover} alt={logement.title} />
+                            <h2>{logement.title}</h2>
+                        </article>
+                    </NavLink>
                 );
             })}
         </section>
@@ -17,7 +20,3 @@ function Cards() {
 }
 
 export default Cards;
-
-/*  - AJOUTER KEY (ID) À CHAQUE ARTICLE
-    - IMPORTER NAVLINK 
-    - AJOUTER UNE NAVLINK TO À CHACHE CARTE AVEC SON ID */
