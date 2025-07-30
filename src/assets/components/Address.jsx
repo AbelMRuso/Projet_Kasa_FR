b5fcb7b0c242d44e65137d02b5180b5e57460758
MrAbelDev <abelmruso@gmail.com>
2025-07-30 17:53:54 +0200

    fin du projet

--- src/assets/components/Address.jsx
+++ src/assets/components/Address.jsx
@@ -1,5 +1,4 @@
-import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home";
 import About from "../../pages/About";
 import FicheLogement from "../../pages/FicheLogement";
